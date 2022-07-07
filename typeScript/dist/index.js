6948a19584235351cc08fad42ab99e7cd3bc94c7
Yuksel Ertem <yuksel.ertem123@gmail.com>
2022-07-07 16:41:37 +0300

    age ve age için getter ve setter eklendi.

--- typeScript/dist/index.js
+++ typeScript/dist/index.js
@@ -1,14 +1,25 @@
 "use strict";
 class Person {
-    constructor(name, lastname) {
-        this.fullName = () => {
-            console.log(`Person name is ${this.name} ${this.lastname}`);
-        };
+    constructor(name, lastname, age) {
         this.name = name;
         this.lastname = lastname;
+        this.age = age;
+        this.fullName = () => {
+            console.log(`Person name is ${this.name} ${this.lastname} and age ${this.age}`);
+        };
+    }
+    get getage() {
+        return this.age;
+    }
+    set setage(value) {
+        if (value === null)
+            throw new Error("Please Enter a age value");
+        else
+            this.age = value;
     }
 }
 let newPerson = new Person("Yuksel", "Ertem");
 let newPerson2 = new Person("Görkem", "Aslan");
+newPerson2.setage = 27;
 newPerson.fullName();
 newPerson2.fullName();
--- typeScript/src/index.ts
+++ typeScript/src/index.ts
@@ -1,20 +1,25 @@
 class Person  {
     
-    private name:string;
-    private lastname:string;
-    
-    constructor(name:string, lastname:string){
-        this.name       = name;
-        this.lastname   = lastname;
+    constructor(private name?:string, private lastname?:string, private age?:number){
     }
-
-     fullName = ():void => {
-        console.log(`Person name is ${this.name} ${this.lastname}`)
+    fullName = ():void => {
+        console.log(`Person name is ${this.name} ${this.lastname} and age ${this.age}`)
+    }
+    get getage(){
+        return this.age;
+    }
+    set setage(value:number | null){
+        if(value === null)
+           throw new Error("Please Enter a age value");
+        else
+        this.age = value;
     }
 }
 
 let newPerson = new Person("Yuksel", "Ertem");
 let newPerson2 = new Person("Görkem", "Aslan")
+
+newPerson2.setage = 27;
 
 newPerson.fullName();
 newPerson2.fullName();
