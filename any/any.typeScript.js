15310d096fde363910fa2d362f8025968191cbc8
Yuksel Ertem <yuksel.ertem123@gmail.com>
2022-07-11 20:25:32 +0300

    any örneği eklendi

---
+++ any/any.typeScript.js
@@ -0,0 +1,9 @@
+function log(arr) {
+    var length = arr.length;
+    for (var i = 0; i < length; i++) {
+        console.log(arr[i]);
+    }
+    console.log(i);
+}
+//log([1,2,3,4])
+log(['a', 'b', 'c', 'd']);
---
+++ any/any.typeScript.ts
@@ -0,0 +1,10 @@
+function log(arr:any[]){
+    let length = arr.length;
+    for(var i = 0; i < length; i++){
+        console.log(arr[i])
+    }
+    console.log(i);
+}
+
+//log([1,2,3,4])
+log(['a','b','c','d'])
